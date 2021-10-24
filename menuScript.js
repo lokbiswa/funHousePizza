942ea6abddb9410b933254e90d087dbc9870db5d
Lok Shankar <lokbiswa@gmail.com>
2021-10-24 05:19:15 -0500

    fixed menu page

--- menuScript.js
+++ menuScript.js
@@ -1,33 +1,33 @@
 // filter function
 
-function filterDiv(current){
-    if (!e) var e = window.event;
-    activeTab(e.toElement);
-    let element = document.querySelectorAll("div.filterDiv");
-    let x = element.length;
-    for(i= 0; i<x;i++){
-        if(current == "all"){
-            element[i].classList.remove('d-none');
+function filterSelection(current) {
+  if (!e) var e = window.event;
+  activeTab(e.target);
+  let element = document.querySelectorAll("div.filterDiv");
+  let x = element.length;
+  for (i = 0; i < x; i++) {
+    if (current == "all") {
+      element[i].classList.remove("d-none");
+    } else {
+      if (element[i].classList.contains(current)) {
+        element[i].classList.remove("d-none");
+      } else {
+        if (element[i].classList.contains("d-none")) {
+        } else {
+          element[i].classList.add("d-none");
         }
-        else{
-            if (element[i].classList.contains(current)){
-                element[i].classList.remove('d-none');
-            }
-            else{
-                if(element[i].classList.contains('d-none')){
-                }
-                else{
-                    element[i].classList.add('d-none');
-                }
-            }
-
-        }
+      }
     }
+  }
 }
 
-function activeTab(button){
-    let element= document.getElementById('myBtnContainer');
-    let current = element.getElementsByClassName('current');
-    current[0].className = current[0].className.replace(" current", "");
-    button.className += " current";
+function activeTab(currentButton) {
+  let element = document.getElementById("myBtnContainer");
+  let selections = document.querySelectorAll("#myBtnContainer .button");
+  selections.forEach((button) => {
+    if (button.classList.contains("current")) {
+      button.classList.remove("current");
+    }
+  });
+  currentButton.className += " current";
 }
